ac2fcf9c977f2a21007466cbc9b2cce679e0d89c
Sophie <81225116+SophieKoenig@users.noreply.github.com>
2022-05-26 14:49:20 +0200

    clickable to next question

--- script/script.js
+++ script/script.js
@@ -1,24 +1,43 @@
+//video https://www.youtube.com/watch?v=R1S_NhKkvGA
+//minute 11:39
+
 const textElement = document.getElementById("text");
 const optionButtonsElement = document.getElementById("option-buttons");
 
 let state = {};
 
-function startGame() {
+const startGame = () => {
   state = {};
   showTextNode(1);
-}
+};
 
-function showTextNode(textNodeIndex) {
+const showTextNode = (textNodeIndex) => {
   const textNode = textNodes.find((textNode) => textNode.id === textNodeIndex);
   textElement.innerText = textNode.text;
   while (optionButtonsElement.firstChild) {
     optionButtonsElement.removeChild(optionButtonsElement.firstChild);
   }
 
-  textNode.options.forEach(option);
-}
+  textNode.options.forEach((option) => {
+    if (showOption(option)) {
+      const button = document.createElement("button");
+      button.innerText = option.text;
+      button.classList.add("btn");
+      button.addEventListener("click", () => selectOption(option));
+      optionButtonsElement.appendChild(button);
+    }
+  });
+};
 
-function selectOption(option) {}
+const showOption = (option) => {
+  return true;
+};
+
+const selectOption = (option) => {
+  const nextTextNodeId = option.nextText;
+  state = Object.assign(state, option.setState);
+  showTextNode(nextTextNodeId);
+};
 
 const textNodes = [
   {
